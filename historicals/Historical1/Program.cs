b539b5d058a7c55cbebc82f143ea8aa9c91e5007
aguecida <d.agueci1@gmail.com>
2016-09-19 22:06:17 -0400

    Read sample spx file.

--- historicals/Historical1/Program.cs
+++ historicals/Historical1/Program.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using CsvHelper;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace Historical1
 {
@@ -10,7 +14,26 @@ namespace Historical1
     {
         static void Main(string[] args)
         {
+            var spx = ReadFile<Index>(ConfigurationManager.AppSettings["spxDataFile"]);
+            
 
+            foreach (var record in spx.Take(5))
+            {
+                Console.WriteLine("{0}, {1}, {2}, {3}", record.Date, record.Open, record.Close, record.Volume);
+            }
+
+            Thread.Sleep(5000);
+        }
+
+        public static IEnumerable<T> ReadFile<T>(string path)
+        {
+            using (var sr = new StreamReader(path))
+            {
+                // Read file to list
+                var reader = new CsvReader(sr);
+                IEnumerable<T> records = reader.GetRecords<T>().ToList();
+                return records;
+            }
         }
     }
 }
