0d3ab81952b5c45ff85133352214a0c8f34307ea
aguecida <d.agueci1@gmail.com>
2016-09-21 00:02:25 -0400

    Add some comments.

--- historicals/Historical1/Program.cs
+++ historicals/Historical1/Program.cs
@@ -4,8 +4,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Configuration;
 using Historical1.Models;
 
@@ -13,18 +11,27 @@ namespace Historical1
 {
     class Program
     {
+        private static string spxPriceDaily = ConfigurationManager.AppSettings["spxDataFile"];
+        private static string vixDaily = ConfigurationManager.AppSettings["vixDataFile"];
+        private static string volumeDaily = ConfigurationManager.AppSettings["volumeDataFile"];
+
         static void Main(string[] args)
         {
-            var spx = ReadFile<Index>(ConfigurationManager.AppSettings["spxDataFile"]);
-            var vix = ReadFile<Vix>(ConfigurationManager.AppSettings["vixDataFile"]);
-            var vol = ReadFile<Volume>(ConfigurationManager.AppSettings["volumeDataFile"]);
+            var spx = ReadFile<Index>(spxPriceDaily);
+            var vix = ReadFile<Vix>(vixDaily);
+            var vol = ReadFile<Volume>(volumeDaily);
         }
 
+        /// <summary>
+        /// Reads contents of a file into a List
+        /// </summary>
+        /// <typeparam name="T">Type of data file</typeparam>
+        /// <param name="path">Path to a data file</param>
+        /// <returns>A list of daily records</returns>
         public static IEnumerable<T> ReadFile<T>(string path)
         {
             using (var sr = new StreamReader(path))
             {
-                // Read file to list
                 var reader = new CsvReader(sr);
                 IEnumerable<T> records = reader.GetRecords<T>().ToList();
                 return records;
