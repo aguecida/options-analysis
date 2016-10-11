ec14a3779d8aea60baea3641f672bb2da130719a
dagueci <dagueci@upstreamworks.com>
2016-10-10 20:23:02 -0400

    Create new historical proj.

---
+++ historicals/Historical2/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Historical2
+{
+    class Program
+    {
+        private static string spxPriceDaily = ConfigurationManager.AppSettings["spxDataFile"];
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting analysis.");
+
+            var watch = Stopwatch.StartNew();
+
+            // Parse analysis arguments
+            AnalysisParameters param = ParseInputArguments(args);
+
+            // Read data files into Lists
+            var spx = ReadFile<Index>(spxPriceDaily).OrderBy(x => x.Date);
+
+            // Make sure data files are starting at a common date
+            if (spx.First().Date != vix.First().Date && spx.First().Date != vol.First().Date && spx.First().Date != vix.First().Date)
+            {
+                Console.WriteLine("ERROR: Start date of records do not match.");
+            }
+
+            // Calculate VROC for each data entry
+            for (var i = 0; i < vol.Count(); i++)
+            {
+                if (i < param.VrocPeriod)
+                    continue;
+
+                double vroc = GetVROC(vol, i, param.VrocPeriod);
+
+                if (vroc > param.VrocThreshold)
+                {
+                    Console.WriteLine("VROC at {0} on {1}", vroc, vol.ElementAt(i).Date);
+                }
+            }
+
+            watch.Stop();
+
+            Console.WriteLine("Analysis complete. Total execution time: {0}ms", watch.ElapsedMilliseconds);
+            Console.WriteLine("\nPress any key to exit.");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Reads contents of a csv file into a List
+        /// </summary>
+        /// <typeparam name="T">Type of data file</typeparam>
+        /// <param name="path">Path to a data file</param>
+        /// <returns>A list of daily records</returns>
+        public static IEnumerable<T> ReadFile<T>(string path)
+        {
+            using (var sr = new StreamReader(path))
+            {
+                var reader = new CsvReader(sr);
+                IEnumerable<T> records = reader.GetRecords<T>().ToList();
+                return records;
+            }
+        }
+
+        /// <summary>
+        /// Calculates the colume rate of change over n periods
+        /// </summary>
+        /// <param name="vol">Volume data</param>
+        /// <param name="index">Current index of volume data</param>
+        /// <param name="period">Period, in days, for calculating rate of change</param>
+        /// <returns></returns>
+        public static double GetVROC(IEnumerable<Volume> vol, int index, int period)
+        {
+            var currVol = (double)vol.ElementAt(index).TotalVolume;
+            var prevVol = (double)vol.ElementAt(index - period).TotalVolume;
+
+            return ((currVol - prevVol) / prevVol) * 100;
+        }
+
+        public static AnalysisParameters ParseInputArguments(string[] args)
+        {
+            AnalysisParameters param = new AnalysisParameters();
+
+            // Get VROC period
+            if (args.Length > 0)
+            {
+                param.VrocPeriod = Convert.ToInt32(args[0]);
+            }
+
+            // Get VROC threshold
+            if (args.Length > 1)
+            {
+                param.VrocThreshold = Convert.ToDouble(args[1]);
+            }
+
+            Console.WriteLine("\nAnalysis parameters:");
+            Console.WriteLine("VROC period (in days) = {0}", param.VrocPeriod);
+            Console.WriteLine("VROC threshold (percentage) = {0}\n", param.VrocThreshold);
+
+            return param;
+        }
+    }
+}
---
+++ historicals/Historical2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Historical2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Historical2")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1e80bf54-e15e-45cd-a93c-5b4d0c3100b4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
