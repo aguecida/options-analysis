491ad965a7fb1fd8e2ba4236484f0b5b191069b9
dagueci <dagueci@upstreamworks.com>
2016-10-15 22:50:50 -0400

    Add settlement price data file and change historical program to get thursday expiries (if friday is a holiday).

--- historicals/Historical2/Program.cs
+++ historicals/Historical2/Program.cs
@@ -22,20 +22,28 @@ namespace Historical2
             var watch = Stopwatch.StartNew();
 
             // Read data files into Lists
-            var spx = ReadFile<Index>(SpxPriceDaily);
+            var spx = ReadFile<Index>(SpxPriceDaily).OrderBy(x => x.Date);
 
             int? expirationDay = null;
             Index expiration = null;
-            Index open = null;
+            Index expirationThursday = null;
+            Index start = null;
             double periodHigh = 0;
             double periodLow = 0;
             bool foundFirstExpiration = false;
+            bool foundExpiration = false;
 
             foreach (var day in spx)
             {
+                if (day.Date.Day >= 1 && day.Date.Day <= 7)
+                {
+                    // Reset foundExpiration flag during first week of the month
+                    foundExpiration = false;
+                }
+
                 if (expirationDay != null)
                 {
-                    open = day;
+                    start = day;
                     expirationDay = null;
                     periodHigh = day.High;
                     periodLow = day.Low;
@@ -53,15 +61,45 @@ namespace Historical2
                     }
                 }
 
-                if (day.Date.Day >= 15 && day.Date.Day <= 21 && day.Date.DayOfWeek == DayOfWeek.Friday)
+                // Capture the Thursday before the third Friday of the month (in case of holiday)
+                if (day.Date.Day >= 14 && day.Date.Day <= 20)
                 {
-                    expiration = day;
+                    if (day.Date.DayOfWeek == DayOfWeek.Thursday)
+                    {
+                        expirationThursday = day;
+                    }
+                }
+
+                if (day.Date.Day >= 15 && day.Date.Day <= 21)
+                {
+                    if (day.Date.DayOfWeek == DayOfWeek.Friday)
+                    {
+                        foundExpiration = true;
+                        expiration = day;
+                        expirationDay = day.Date.Day;
+
+                        if (foundFirstExpiration)
+                        {
+                            Console.WriteLine("Expiration Date: {0}, Start Date: {1}, Opening Price: {2}, High: {3}, Low: {4}, Expiry Price: {5}, Spread: {6}",
+                                expiration.Date.ToString("dd/MM/yyyy"), start.Date.ToString("dd/MM/yyyy"), Math.Round(start.Open, 2), Math.Round(periodHigh, 2), Math.Round(periodLow, 2), Math.Round(expiration.Open, 2), Math.Round(Math.Abs(expiration.Open - start.Open), 2));
+                        }
+                        else
+                        {
+                            foundFirstExpiration = true;
+                        }
+                    }
+                }
+
+                // If we are passed the third week and still have not found the Friday expiration (it was a holiday), expiration was on the preceeding Thursday
+                if (day.Date.Day > 21 && !foundExpiration)
+                {
+                    foundExpiration = true;
                     expirationDay = day.Date.Day;
 
                     if (foundFirstExpiration)
                     {
-                        Console.WriteLine("Expiration: {0}, High: {1}, Low: {2}, Spread: {3}",
-                            expiration.Date.ToString("dd/MM/yyyy"), Math.Round(periodHigh, 2), Math.Round(periodLow, 2), Math.Round(periodHigh - periodLow, 2));
+                        Console.WriteLine("Expiration Date: {0}, Start Date: {1}, Opening Price: {2}, High: {3}, Low: {4}, Expiry Price: {5}, Spread: {6}",
+                            expirationThursday.Date.ToString("dd/MM/yyyy"), start.Date.ToString("dd/MM/yyyy"), Math.Round(start.Open, 2), Math.Round(periodHigh, 2), Math.Round(periodLow, 2), Math.Round(expirationThursday.Open, 2), Math.Round(Math.Abs(expirationThursday.Open - start.Open), 2));
                     }
                     else
                     {
@@ -74,7 +112,7 @@ namespace Historical2
 
             Console.WriteLine("Analysis complete. Total execution time: {0}ms", watch.ElapsedMilliseconds);
             Console.WriteLine("\nPress any key to exit");
-            Console.ReadKey();
+            Console.ReadLine();
         }
 
         /// <summary>
